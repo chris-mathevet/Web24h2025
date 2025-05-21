c1716808c27dfaea84e46f1fe76ba9bfb0737cbe
chris-mathevet <chris.mathevet@etu.univ-orleans.fr>
2025-05-21 12:03:33 +0200

    commit init

---
+++ index.php
@@ -0,0 +1,11 @@
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <!-- <meta name="viewport" content="width=device-width" /> -->
+    <title>24h Info Web 2025</title>
+  </head>
+  <body>
+    <h1>C'est les 24h de l'INFO !!!!!</h1>
+    <img src="assets/img/julian.png" alt="My test image" />
+  </body>
+</html>
